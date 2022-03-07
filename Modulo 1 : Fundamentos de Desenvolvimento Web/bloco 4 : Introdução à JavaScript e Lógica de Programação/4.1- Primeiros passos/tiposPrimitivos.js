7f03a8b47294bc4e8c9c8f7012db4aba16cf8add
Jeferson Gomes <jeferson_gjr@hotmail.com>
2022-03-07 18:26:00 -0300

    exercicios sobre tipos primitivos

---
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.1- Primeiros passos/tiposPrimitivos.js
@@ -0,0 +1,17 @@
+/*let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com'; */
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+let perimeter = (2*base) + (2*height);
+
+console.log(perimeter);
---
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.1- Primeiros passos/variaveis.js
@@ -0,0 +1,13 @@
+let myName = 'Jeferson';
+const birthCity = 'Rio de Janeiro';
+let birthYear = 1991;
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030;
+console.log(birthYear);
+
+/*birthCity = "São Paulo"
+console.log(birthCity) */
