7394a38d875cf7101667e0b6f539cca94c3f9e85
Jeferson Gomes <jeferson_gjr@hotmail.com>
2022-03-09 20:49:47 -0300

    terminadno os exercicis

--- Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/exercicios4-2-parte2.js
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/exercicios4-2-parte2.js
@@ -2,6 +2,8 @@
 let numbers =[]
 for(let index = 1; index <= 25; index +=1){
     numbers[index] = index;
-}
-console.log(numbers);
+};
 
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2);
+  };
