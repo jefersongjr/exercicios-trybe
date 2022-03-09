3e48194f5f7faea16b20ea1afa427fa7fcf106f8
Jeferson Gomes <jeferson_gjr@hotmail.com>
2022-03-09 15:52:00 -0300

    verificando maior numero no array

--- Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/Exercicio4-2.js
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/Exercicio4-2.js
@@ -1,15 +1,36 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+/*1 - imprimir o array */
 for(let index=0; index < numbers.length ; index += 1 ){
     console.log(numbers[index]);
-}/*imprimir o array */
+}
 
+/*2 - percorra todo array e some os elementos*/
 let soma = 0;
 
 for(index = 0 ; index <numbers.length; index += 1){
     soma += numbers[index];
 }
-console.log(soma); /* percorra todo array e some os elementos*/
+console.log(soma); 
 
+/* 3 -Faça a média aritmetica */
 let mediaAritmetica = soma/(numbers.length);
-console.log(mediaAritmetica);  /*Faça a média aritmetica */
+console.log(mediaAritmetica);  
+
+/*4- mostre se a média é menor ou maior que 20 */
+
+if(mediaAritmetica >20){
+    console.log('Valor maior 20');
+}else {
+    console.log('valor menor ou igual a 20');
+}
+
+/* 5 - utilizando for imprima o maior número do array */
+let maiorNumero =0;
+for(index = 0 ; index <numbers.length; index += 1){
+    if(maiorNumero < numbers[index]){
+        maiorNumero = numbers[index] ;
+    }else {
+        maiorNumero = maiorNumero;
+    } 
+}
+console.log(maiorNumero);
