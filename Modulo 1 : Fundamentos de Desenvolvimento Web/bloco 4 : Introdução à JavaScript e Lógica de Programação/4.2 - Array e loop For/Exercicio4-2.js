a862346c5415ea184d6849adc456f4e653d11b55
Jeferson Gomes <jeferson_gjr@hotmail.com>
2022-03-09 15:28:06 -0300

    exercicio 2 somando os itens do array

--- Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/Exercicio4-2.js
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 4 : Introdução à JavaScript e Lógica de Programação/4.2 - Array e loop For/Exercicio4-2.js
@@ -2,4 +2,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let index=0; index < numbers.length ; index += 1 ){
     console.log(numbers[index]);
-}/*1 - percorrer o array e imprimir */
+}/*imprimir o array */
+
+let soma = 0;
+
+for(index = 0 ; index <numbers.length; index += 1){
+    soma += numbers[index];
+}
+console.log(soma); /* percorra todo array e some os elementos*/
