003606543c17bef787c289ac5a9ae81485700e55
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-13 15:17:05 -0300

    exercicio 1 -reduce

---
+++ Modulo 1 : Fundamentos de Desenvolvimento Web/bloco 8 - Higher Order Functions do JavaScript ES6/8.4 HOF - reduce/exercise1.js
@@ -0,0 +1,12 @@
+//Juntuando o array de arrays em 1 só
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+
+console.log(flatten());
