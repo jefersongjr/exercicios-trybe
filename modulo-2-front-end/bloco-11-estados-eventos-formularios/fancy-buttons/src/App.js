21d7deb9caa604e076a30ec4961543be5f51338c
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-11 12:33:46 -0300

    exercicios 11.1- para fixar

--- modulo-2-front-end/bloco-11-estados-eventos-formularios/fancy-buttons/src/App.js
+++ modulo-2-front-end/bloco-11-estados-eventos-formularios/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    chave1: 0,
+    chave2: 0,
+    chave3:0
+  }
+
+  handleClick1 = () => {
+    this.setState({chave1: 1})
+  }
+  
+  handleClick2 = () => {
+    this.setState({chave2: 1})
+  }
+  
+  handleClick3 = () => {
+    this.setState({chave1: 1})
+  }
+  
+  render(){
+    return (
+      <div>
+        <button className='button' onClick={this.handleClick1}>Botão1</button>
+        <button className='button' onClick={this.handleClick2}>Botão2</button>
+        <button className='button' onClick={this.handleClick3}>Botão3</button>
+      </div>
+    );
+  }
 }
 
 export default App;
