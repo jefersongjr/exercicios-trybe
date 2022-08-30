4d3d3b5eb31a9f887be4dacb3e4f5a1fb6a0bfa2
jefersongjr <jeferson_gjr@hotmail.com>
2022-08-30 14:38:37 -0300

    feat: criando calculador BMI com node

---
+++ modulo-3-back-end/bloco-22-introducao-ao-node/dia-1.1-introducao-node/atividades-1.1/BMI/bmi.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+
+// function handleBMI(weight, height) {
+//   console.log(`Weight: ${weight}, Height: ${height}`);
+
+//   const heightInMeters = height / 100;
+//   const heightSquared = heightInMeters ** 2;
+
+//   const bmi = (weight / heightSquared);
+  
+//   return bmi;
+// }
+
+// function main() {
+   const weight = readline.questionInt('What\'s your weight? (kg) ');
+   const height = readline.questionInt('What\'s your height? (cm) ');
+  
+   const bmi = handleBMI(weight, height);
+  
+//   console.log(`BMI: ${bmi.toFixed(2)}`);
+// }
+
+// main();
